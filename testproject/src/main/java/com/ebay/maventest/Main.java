2fcf6ef52ab745d4c17c7fc82fe49798aad107b0
Luyao Xiong <you@example.com>
2022-08-09 01:08:40 +0800

    init commit

---
+++ testproject/src/main/java/com/ebay/maventest/Main.java
@@ -0,0 +1,14 @@
+package com.ebay.maventest;
+
+/**
+ * Class: Main
+ * Date: 6/30/22 9:02 AM
+ * Description: TODO
+ *
+ * @author luyxiong
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
